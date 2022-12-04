fe06788f40eec85a9d0ab01ec47147e77f3f5618
Pedro Stanzani <pedrohstanzani@gmail.com>
2022-12-04 09:34:09 -0300

    Add search filter and filter badge activation

---
+++ assets/js/index.js
@@ -0,0 +1,45 @@
+const main = () => {
+  const searchInput = document.querySelector('#job-search-input');
+  const unorderedList = document.querySelector('#job-listings');
+
+  const listItems = Array.from(unorderedList.querySelectorAll('li')).map(item => {
+    const jobTitle = item.querySelector('.job-title').innerText;
+    const companyName = item.querySelector('.company-name').innerText;
+    const query = jobTitle + ' ' + companyName;
+
+    return {
+      query: query.toLowerCase(),
+      item: item
+    };
+  })
+
+  searchInput.addEventListener('input', () => {
+    const query = searchInput.value.toLowerCase();
+    const filteredItems = listItems.filter(item => item.query.includes(query))
+    unorderedList.replaceChildren(...filteredItems.map(i => i.item));
+  })
+}
+
+const activeFilterButtons = () => {
+  const buttons = document.querySelectorAll('.filter-badge');
+
+  buttons.forEach(button => {
+    button.addEventListener('click', () => {
+      const isActive = Array
+        .from(button.classList)
+        .includes('filter-badge-active');
+
+      if (isActive) {
+        button.classList.remove('filter-badge-active');
+      } else {
+        button.classList.add('filter-badge-active');
+      }
+    })
+  })
+}
+
+// Main script execution
+addEventListener('DOMContentLoaded', () => {
+  main();
+  activeFilterButtons();
+})
