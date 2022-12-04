28dab9febefd4733b7433fe9314498c7e9667144
Pedro Stanzani <pedrohstanzani@gmail.com>
2022-12-04 10:55:47 -0300

    Add menu go back functionality

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,22 +2,24 @@ const main = () => {
   const searchInput = document.querySelector('#job-search-input');
   const unorderedList = document.querySelector('#job-listings');
 
-  const listItems = Array.from(unorderedList.querySelectorAll('li')).map(item => {
-    const jobTitle = item.querySelector('.job-title').innerText;
-    const companyName = item.querySelector('.company-name').innerText;
-    const query = jobTitle + ' ' + companyName;
-
-    return {
-      query: query.toLowerCase(),
-      item: item
-    };
-  })
-
-  searchInput.addEventListener('input', () => {
-    const query = searchInput.value.toLowerCase();
-    const filteredItems = listItems.filter(item => item.query.includes(query))
-    unorderedList.replaceChildren(...filteredItems.map(i => i.item));
-  })
+  if (searchInput && unorderedList) {
+    const listItems = Array.from(unorderedList.querySelectorAll('li')).map(item => {
+      const jobTitle = item.querySelector('.job-title').innerText;
+      const companyName = item.querySelector('.company-name').innerText;
+      const query = jobTitle + ' ' + companyName;
+  
+      return {
+        query: query.toLowerCase(),
+        item: item
+      };
+    })
+  
+    searchInput.addEventListener('input', () => {
+      const query = searchInput.value.toLowerCase();
+      const filteredItems = listItems.filter(item => item.query.includes(query))
+      unorderedList.replaceChildren(...filteredItems.map(i => i.item));
+    })
+  }
 }
 
 const activeFilterButtons = () => {
@@ -38,8 +40,19 @@ const activeFilterButtons = () => {
   })
 }
 
+const goBack = () => {
+  let closeMenu = document.querySelector('#close-menu');
+
+  if (closeMenu) {
+    closeMenu.addEventListener('click', () => {
+      history.back();
+    })
+  }
+}
+
 // Main script execution
 addEventListener('DOMContentLoaded', () => {
   main();
   activeFilterButtons();
+  goBack();
 })
